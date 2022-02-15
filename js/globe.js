d915f7daa0b756892c9721cfe3028d957b5cc21e
Jacob Pech <pechjacob@gmail.com>
2022-02-15 09:45:36 -0700

    sdfg

--- js/globe.js
+++ js/globe.js
@@ -99,7 +99,7 @@ DAT.Globe = function(container, opts) {
     h = container.offsetHeight || window.innerHeight;
 
     // camera & scene {{{
-    camera = new THREE.PerspectiveCamera(45, w / h, 1, 20000);
+    camera = new THREE.PerspectiveCamera(35, w / h, 1, 20000);
     camera.position.z = distance;
 
     // make China faced to user
